6d2e34d48e039672bbb3a8669253e7a09268ce00
James Musyoka <jamesmu475@gmail.com>
2019-01-18 09:27:19 +0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { GOAL2DetailsComponent } from './goal2-details/goal2-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GOAL2FormComponent } from './goal2-form/goal2-form.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { GOAL2FormComponent } from './goal2-form/goal2-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
